2a6cc08bba3ac38d88b8468d018b890244f8f06b
u0879939RunLi <lirunliruntc@gmail.com>
2017-11-09 11:36:32 -0700

    barcharts draft

--- project/js/usmap.js
+++ project/js/usmap.js
@@ -8,7 +8,7 @@ class Usmap{
         let airports = this.airports;
 
 
-        d3.select("#map").selectAll("svg").remove();
+        d3.select("body").selectAll("svg").remove();
 
         let width = 960;
         let height = 600;
@@ -19,6 +19,7 @@ class Usmap{
 
         let path = d3.geo.path()
             .projection(projection);
+
 
         let svg = d3.select("#map")
             .append("svg")
@@ -34,10 +35,7 @@ class Usmap{
                 .enter()
                 .append("path")
                 .attr("d", path)
-                .style("stroke", "#fff")
-                .style("stroke-width", "1")
-                .style("fill", "steelBlue")
-                .style("opacity", 0.2);
+                .attr("class", "mappath");
 
             });
         let g2 = svg.append("g");
@@ -52,6 +50,49 @@ class Usmap{
             .attr("cy", function (d) {return projection([d.lon, d.lat])[1];})
             .attr("r", 4)
             .attr("class", "spot");
+
+        let barChart1 = d3.select("#map").append("svg")
+            .attr("width", 300)
+            .attr("height", 220)
+            .attr("id", "top10");
+        barChart1.append("text").text("TOP 10 busiest airports")
+            .attr("x", 20)
+            .attr("y", 20);
+
+        let g5 = barChart1.append("g");
+        g5.selectAll("rect").data(airports.slice(0,10))
+            .enter().append("rect")
+            .attr("x", 20)
+            .attr("y", function(d, i){return i*20 + 40;})
+            .attr("width", function(d){return d.flightcount/150;})
+            .attr("height", 20)
+            .attr("opacity", function(d){return d.flightcount/25000})
+            .attr("class", "bar1");
+        g5.selectAll("text").data(airports.slice(0, 10))
+            .enter().append("text")
+            .attr("x", 20)
+            .attr("y", function(d, i){return i*20 + 56;})
+            .text(function(d){return d.IATA});
+        let g6 = barChart1.append("g");
+        g6.selectAll("text").data(airports.slice(0, 10))
+            .enter().append("text")
+            .attr("x", function(d){return d.flightcount/150 + 20;})
+            .attr("y", function(d, i){return i*20 + 56;})
+            .text(function(d){return d.flightcount;});
+
+        let weekChart = d3.select("#map").append("svg")
+            .attr("width", 160)
+            .attr("height", 600)
+            .attr("id", "week")
+            .attr("transform", "translate(0, 0) scale(1,-1)");
+        let MonthChart = d3.select("#map").append("svg")
+            .attr("width", 260)
+            .attr("height", 600)
+            .attr("id", "month");
+        let HourChart = d3.select("#map").append("svg")
+            .attr("width", 400)
+            .attr("height", 500)
+            .attr("id", "hour");
 
         locations.on("click", function(d){
             g2.selectAll("text").remove();
@@ -78,73 +119,68 @@ class Usmap{
                     .attr("y1", function () {return projection([d.lon, d.lat])[1];})
                     .attr("x2", function(a) {return a[0];})
                     .attr("y2", function(a) {return a[1];})
-                    .style("stroke", "green")
-                    .style("stroke-width", 2)
-                    .attr("opacity", 0.3);
+                    .attr("class", "route");
             });
-        });
-        //
-        // let g4 = svg.append("g").attr("class", "hovered");
-        // locations.on("mouseover", function(d){
-        //     g4.append("text")
-        //         .text(function(){return d.name;})
-        //         .attr("x", function () {return projection([d.lon, d.lat])[0];})
-        //         .attr("y", function () {return projection([d.lon, d.lat])[1] + 20;});
-        //
-        //     d3.csv("dataset/airportconnection.csv", function(connections){
-        //         let airportconnection = connections.filter(function(c){
-        //             return c.ORIGIN == d.IATA;
-        //         });
-        //         let routes = [];
-        //         for(let i = 0; i < airportconnection.length; i++){
-        //             let dest = airports.filter(function(a){
-        //                 return a.IATA == airportconnection[i].DEST;
-        //             })
-        //             if(dest.length > 0) routes.push(projection([dest[0].lon, dest[0].lat]));
-        //         }
-        //         g4.selectAll("line").data(routes).enter().append("line")
-        //             .attr("x1", function () {return projection([d.lon, d.lat])[0];})
-        //             .attr("y1", function () {return projection([d.lon, d.lat])[1];})
-        //             .attr("x2", function(a) {return a[0];})
-        //             .attr("y2", function(a) {return a[1];})
-        //             .style("stroke", "darkBlue")
-        //             .style("stroke-width", 2)
-        //             .attr("opacity", 0.3);
-        //     });
-        // });
-        // locations.on("mouseout", function(){
-        //     g4.selectAll("text").remove();
-        //     g4.selectAll("line").remove();
-        // });
-        //
-        let barChart1 = d3.select("#map").append("svg")
-            .attr("width", 300)
-            .attr("height", 550)
-            .attr("id", "top10");
-        barChart1.append("text").text("TOP 10 busiest airports")
-            .attr("x", 20)
-            .attr("y", 20);
 
-        let g5 = barChart1.append("g");
-        g5.selectAll("rect").data(airports.slice(0,10))
-            .enter().append("rect")
-            .attr("x", 20)
-            .attr("y", function(d, i){return i*20 + 40;})
-            .attr("width", function(d){return d.flightcount/150;})
-            .attr("height", 20)
-            .attr("opacity", function(d){return d.flightcount/25000})
-            .attr("class", "bar1");
-        g5.selectAll("text").data(airports.slice(0, 10))
-            .enter().append("text")
-            .attr("x", 20)
-            .attr("y", function(d, i){return i*20 + 56;})
-            .text(function(d){return d.IATA});
-        let g6 = barChart1.append("g");
-        g6.selectAll("text").data(airports.slice(0, 10))
-            .enter().append("text")
-            .attr("x", function(d){return d.flightcount/150 + 20;})
-            .attr("y", function(d, i){return i*20 + 56;})
-            .text(function(d){return d.flightcount;});
+            let week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+            let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            let hour = ["00", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+            weekChart.selectAll("rect").remove();
+            MonthChart.selectAll("rect").remove();
+            HourChart.selectAll("rect").remove();
+
+            for(let i = 0; i < week.length; i++){
+                d3.csv("dataset/" + week[i] + ".csv", function(data){
+                    console.log(data);
+                    for(let j = 0; j < data.length; j++){
+                        if(d.IATA == data[j].Origin){
+                            weekChart.append("rect")
+                                .attr("x", i*20 + 10)
+                                .attr("y",20)
+                                .attr("width", 20)
+                                .attr("height", data[j].FlightCount/100)
+                                .attr("class", "bar1");
+                        }
+                    }
+                });
+            }
+
+            for(let i = 0; i < month.length; i++){
+                d3.csv("dataset/" + month[i] + ".csv", function(data){
+                    console.log(data);
+                    for(let j = 0; j < data.length; j++){
+                        if(d.IATA == data[j].Origin){
+                            MonthChart.append("rect")
+                                .attr("x", i*20 + 10)
+                                .attr("y",20)
+                                .attr("width", 20)
+                                .attr("height", data[j].FlightCount/100)
+                                .attr("class", "bar1");
+                        }
+                    }
+                });
+            }
+
+            for(let i = 0; i < hour.length; i++){
+                d3.csv("dataset/" + hour[i] + ".csv", function(data){
+                    console.log(data);
+                    for(let j = 0; j < data.length; j++){
+                        if(d.IATA == data[j].Origin){
+                            HourChart.append("rect")
+                                .attr("x", i*20 + 10)
+                                .attr("y",20)
+                                .attr("width", 20)
+                                .attr("height", data[j].FlightCount/100)
+                                .attr("class", "bar1");
+                        }
+                    }
+                });
+            }
+
+
+        });
+
+
 
     }
 }
